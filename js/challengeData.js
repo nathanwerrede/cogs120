1b7e6ffae5b4deea4cc3f568f6ff1229cf0cf26d
nathanwerrede <nathanwerrede@gmail.com>
2018-11-08 15:41:39 -0800

    updated website with scripts

---
+++ js/challengeData.js
@@ -0,0 +1,17 @@
+var games = {'Mario' : [{'ChallengeTitle' : 'beat the game without dying',
+                         'ChallengeDescription' : 'dont die in the game',
+                         'difficulty' : 'border-warning'},
+                        {'ChallengeTitle' : 'beat the game without breathing',
+                         'ChallengeDescription' : 'dont breathe',
+                         'difficulty' : 'border-danger'}],
+             'Fortnite' : [{'ChallengeTitle' : 'win without building',
+                            'ChallengeDescription' : 'dont build',
+                            'difficulty' : 'border-danger'}, {'ChallengeTitle':
+                          'Mining for Wins', 'ChallengeDescription':'Win a game
+                          without picking up a weapon', 'difficulty':'border-danger'}],
+              'NBA 2K19' : [{'ChallengeTitle': 'Stretch 5', 'ChallengeDescription':
+                          'Make more than 5 3-pointers in a game with any center',
+                          'difficulty': 'border-success'},{'ChallengeTitle': "Mercy Rule",
+                          'ChallengeDescription': 'Win a game by more than 50 points',
+                          'difficulty':'border-warning'} ]
+            }
